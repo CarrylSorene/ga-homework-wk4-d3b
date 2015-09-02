64d1f2a0a469889bfc562febdf83fc9fd60883ad
Carryl Sorene <carrylsorene@gmail.com>
2015-09-02 20:33:53 +0100

    Basic calculator

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ post '/' do
     when '-' then @home = form_input1 = params[:number1].to_f - form_input2 = params[:number2].to_f
     when '*' then @home = form_input1 = params[:number1].to_f * form_input2 = params[:number2].to_f
     when '/' then @home = form_input1 = params[:number1].to_f / form_input2 = params[:number2].to_f
+    
     else erb :home
     end
   erb :home
-  end
+end
